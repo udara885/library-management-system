2c52e197cc7c7132275418c595fe019101745451
Udara Lakshan <ulakshan061@gmail.com>
2025-02-07 11:03:41 +0530

    add book management functions: create, update, and delete book entries

--- backend/controller/book.controller.ts
+++ backend/controller/book.controller.ts
@@ -11,3 +11,66 @@ export const getBooks = async (req: any, res: any) => {
 	}
 }
 
+export const addBook = async (req: any, res: any) => {
+	const book = req.body
+
+	if (
+		!book.title ||
+		!book.author ||
+		!book.category ||
+		!book.publicationYear
+	) {
+		return res
+			.status(400)
+			.json({ success: false, message: "Please fill all the fields" })
+	}
+
+	const newBook = new Book(book)
+
+	try {
+		await newBook.save()
+		res.status(201).json({ success: true, data: newBook })
+	} catch (error: any) {
+		console.error(`Error in addBook: ${error.message}`)
+		res.status(500).json({ success: false, message: "Server Error" })
+	}
+}
+
+export const updateBook = async (req: any, res: any) => {
+	const { id } = req.params
+	const book = req.body
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res
+			.status(404)
+			.json({ success: false, message: "Invalid Book ID" })
+	}
+
+	try {
+		const updatedBook = await Book.findByIdAndUpdate(id, book, {
+			new: true,
+		})
+		res.status(200).json({ success: true, data: updatedBook })
+	} catch (error: any) {
+		console.error(`Error in updatebook: ${error.message}`)
+		res.status(500).json({ success: false, message: "Server Error" })
+	}
+}
+
+export const deleteBook = async (req: any, res: any) => {
+	const { id } = req.params
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res
+			.status(404)
+			.json({ success: false, message: "Invalid Book ID" })
+	}
+
+	try {
+		await Book.findByIdAndDelete(id)
+		res.status(200).json({ success: true, message: "Book deleted" })
+	} catch (error: any) {
+		console.error(`Error in deleteBook: ${error.message}`)
+		res.status(500).json({ success: false, message: "Server Error" })
+	}
+}
