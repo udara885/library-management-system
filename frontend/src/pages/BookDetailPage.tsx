51f505732a4491f57cf223dc3778b8ef81a50437
Udara Lakshan <ulakshan061@gmail.com>
2025-02-11 18:49:11 +0530

    refactor BookDetailPage for improved admin functionality and layout

--- frontend/src/pages/BookDetailPage.tsx
+++ frontend/src/pages/BookDetailPage.tsx
@@ -1,17 +1,24 @@
 import { useNavigate, useParams } from "react-router"
 import { useBookStore } from "../store/book"
-import { useContext, useEffect, useState } from "react"
-import { Book } from "../types/types"
-import { Edit, Trash } from "lucide-react"
+import { useContext } from "react"
+import { BookUp, BookX } from "lucide-react"
 import { Link } from "react-router"
 import { AdminContext } from "../context/adminContext"
 import toast from "react-hot-toast"
 
 const BookDetailPage = () => {
-	const [book, setBook] = useState<Book | undefined>()
 	const { id } = useParams()
+
 	const { getBook, deleteBook } = useBookStore()
+
+	if (!id) {
+		throw new Error("id is undefined")
+	}
+
+	const book = getBook(id)
+
 	const adminContext = useContext(AdminContext)
+
 	const navigate = useNavigate()
 
 	if (!adminContext) {
@@ -19,12 +26,6 @@ const BookDetailPage = () => {
 	}
 
 	const { isAdmin } = adminContext
-
-	useEffect(() => {
-		if (id) {
-			setBook(getBook(id))
-		}
-	}, [getBook, id])
 
 	const handleDelete = async (id: string | undefined): Promise<void> => {
 		if (!id) {
@@ -40,14 +41,14 @@ const BookDetailPage = () => {
 	}
 
 	return (
-		<div className="max-w-screen-xl mx-auto flex flex-col sm:flex-row sm:justify-between p-10">
-			<div className="flex justify-center">
+		<div className="max-w-screen-xl mx-auto flex flex-col md:flex-row md:justify-between p-10 gap-5">
+			<div className="flex justify-center md:w-1/3">
 				<img
 					src={book?.image}
 					alt={book?.title}
 				/>
 			</div>
-			<div className="flex flex-col gap-5 text-gray-200 pt-5">
+			<div className="flex flex-col gap-5 text-gray-200 md:w-2/3">
 				<h1 className="text-3xl font-bold">{book?.title}</h1>
 				<h2 className="text-lg italic text-gray-400">
 					By {book?.author}
@@ -58,19 +59,23 @@ const BookDetailPage = () => {
 				</h4>
 				<p className="text-lg text-justify">{book?.description}</p>
 				{isAdmin && (
-					<div className="flex justify-between">
-						<Link to={`/admin/update-book/${book?._id}`}>
-							<button className="p-2 w-32 bg-blue-400  rounded-lg font-bold text-gray-900 flex items-center justify-center">
-								<Edit />
-								Update
+					<div className="flex justify-between w-full gap-10">
+						<div className="w-1/2">
+							<Link to={`/admin/update-book/${book?._id}`}>
+								<button className="p-2 bg-blue-400 rounded-lg font-bold text-gray-900 flex items-center justify-center cursor-pointer w-full">
+									<BookUp />
+									Update
+								</button>
+							</Link>
+						</div>
+						<div className="w-1/2">
+							<button
+								className="p-2 bg-red-400 rounded-lg font-bold text-gray-900 flex items-center justify-center cursor-pointer w-full"
+								onClick={() => handleDelete(book?._id)}
+							>
+								<BookX /> Delete
 							</button>
-						</Link>
-						<button
-							className="p-2 w-32 bg-red-400 rounded-lg font-bold text-gray-900 flex items-center justify-center"
-							onClick={() => handleDelete(book?._id)}
-						>
-							<Trash /> Delete
-						</button>
+						</div>
 					</div>
 				)}
 			</div>
