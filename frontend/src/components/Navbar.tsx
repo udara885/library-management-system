f2fd0c338ffce90c595385598572c3d9d956137e
Udara Lakshan <ulakshan061@gmail.com>
2025-02-11 18:48:56 +0530

    add book addition button in Navbar for admin users

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { CircleUserRound, LogOut } from "lucide-react"
+import { BookPlus, CircleUserRound, LogOut } from "lucide-react"
 import { Link } from "react-router"
 import { AdminContext } from "../context/adminContext"
 import { useContext } from "react"
@@ -13,20 +13,27 @@ const Navbar = () => {
 		<div className="p-5 border-b border-gray-800 max-w-screen-xl mx-auto">
 			<div className="flex items-center justify-between text-gray-200">
 				<Link to={`${isAdmin ? "/admin" : "/"}`}>
-					<h1 className="font-bold text-xl md:text-2xl">
-						{isAdmin
-							? "Admin Dashboard"
-							: "Library Management System"}
+					<h1 className="font-bold text-2xl md:text-3xl">
+						{isAdmin ? "Admin Dashboard" : "Home"}
 					</h1>
 				</Link>
-				<Link to={`${isAdmin ? "/" : "/admin"}`}>
-					<button
-						onClick={() => setIsAdmin(!isAdmin)}
-						className="border-gray-600 border p-2 rounded-lg font-semibold"
-					>
-						{isAdmin ? <LogOut /> : <CircleUserRound />}
-					</button>
-				</Link>
+				<div className="flex gap-2 items-center">
+					{isAdmin && (
+						<Link to="/admin/add-book">
+							<button className="border-gray-600 border p-1 md:p-2 rounded-lg font-semibold cursor-pointer">
+								<BookPlus />
+							</button>
+						</Link>
+					)}
+					<Link to={`${isAdmin ? "/" : "/admin"}`}>
+						<button
+							onClick={() => setIsAdmin(!isAdmin)}
+							className="border-gray-600 border p-1 md:p-2 rounded-lg font-semibold cursor-pointer"
+						>
+							{isAdmin ? <LogOut /> : <CircleUserRound />}
+						</button>
+					</Link>
+				</div>
 			</div>
 		</div>
 	)
