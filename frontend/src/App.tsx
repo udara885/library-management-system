02b0b0da0d3c8e8fb9dc6cf6c31e7d13db027bd7
Udara Lakshan <ulakshan061@gmail.com>
2025-02-11 18:48:50 +0530

    add admin route handling in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import Navbar from "./components/Navbar"
-import { Route, Routes } from "react-router"
+import { Route, Routes, useLocation } from "react-router"
 import HomePage from "./pages/HomePage"
 import AdminPage from "./pages/AdminPage"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import AddPage from "./pages/AddPage"
 import UpdatePage from "./pages/UpdatePage"
 import BookDetailPage from "./pages/BookDetailPage"
@@ -10,7 +10,15 @@ import { AdminContext } from "./context/adminContext"
 import { Toaster } from "react-hot-toast"
 
 function App() {
-	const [isAdmin, setIsAdmin] = useState(false)
+	const [ isAdmin, setIsAdmin ] = useState( false )
+
+	const location = useLocation()
+	
+	useEffect(() => {
+		if (location.pathname === "/admin") {
+			setIsAdmin(true)
+		}
+	}, [location.pathname])
 
 	return (
 		<div className="bg-gray-900 min-h-screen">
@@ -23,7 +31,7 @@ function App() {
 						element={<HomePage />}
 					/>
 					<Route
-						path="book/:id"
+						path="/book/:id"
 						element={<BookDetailPage />}
 					/>
 					<Route
