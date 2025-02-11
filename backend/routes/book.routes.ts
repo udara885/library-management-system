3a31b4b943741badc64697fb85266dad8358d34c
Udara Lakshan <ulakshan061@gmail.com>
2025-02-12 01:00:02 +0530

    add: implement getBook route to retrieve a book by ID

--- backend/routes/book.routes.ts
+++ backend/routes/book.routes.ts
@@ -2,13 +2,15 @@ import express from "express"
 import {
 	addBook,
 	deleteBook,
+	getBook,
 	getBooks,
 	updateBook,
 } from "../controller/book.controller"
 
 const router = express.Router()
 
-router.get("/books", getBooks)
+router.get( "/books", getBooks )
+router.get("/book/:id", getBook)
 router.post("/add-book", addBook)
 router.put("/update-book/:id", updateBook)
 router.delete("/delete-book/:id", deleteBook)
