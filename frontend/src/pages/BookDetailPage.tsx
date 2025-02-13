b764eccd6d8502f30b3110c03c257e5cced63627
Udara Lakshan <ulakshan061@gmail.com>
2025-02-13 09:28:34 +0530

    fix: add loading state and Loader component to BookDetailPage for improved user experience

--- frontend/src/pages/BookDetailPage.tsx
+++ frontend/src/pages/BookDetailPage.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router"
 import { AdminContext } from "../context/adminContext"
 import toast from "react-hot-toast"
 import { Book } from "../types/types"
+import Loader from "../components/Loader"
 
 const BookDetailPage = () => {
 	const { id } = useParams()
@@ -14,14 +15,18 @@ const BookDetailPage = () => {
 
 	const [book, setBook] = useState<Book>()
 
+	const [loading, setLoading] = useState(true)
+
 	if (!id) {
 		throw new Error("id is undefined")
 	}
 
 	useEffect(() => {
 		const fetchBook = async () => {
+			setLoading(true)
 			const { data } = await getBook(id)
 			setBook(data)
+			setLoading(false)
 		}
 		fetchBook()
 	}, [getBook, id])
@@ -50,45 +55,55 @@ const BookDetailPage = () => {
 	}
 
 	return (
-		<div className="max-w-screen-xl mx-auto flex flex-col md:flex-row md:justify-between p-10 gap-5">
-			<div className="flex justify-center md:w-1/3">
-				<img
-					src={book?.image}
-					alt={book?.title}
-				/>
-			</div>
-			<div className="flex flex-col gap-5 text-gray-200 md:w-2/3">
-				<h1 className="text-3xl font-bold">{book?.title}</h1>
-				<h2 className="text-lg italic text-gray-400">
-					By {book?.author}
-				</h2>
-				<h3 className="text-lg">Category : {book?.category}</h3>
-				<h4 className="text-lg">
-					Published Year : {book?.publicationYear}
-				</h4>
-				<p className="text-lg text-justify">{book?.description}</p>
-				{isAdmin && (
-					<div className="flex justify-between w-full gap-10">
-						<div className="w-1/2">
-							<Link to={`/admin/update-book/${book?._id}`}>
-								<button className="p-2 bg-blue-400 rounded-lg font-bold text-gray-900 flex items-center justify-center cursor-pointer w-full">
-									<BookUp />
-									Update
-								</button>
-							</Link>
-						</div>
-						<div className="w-1/2">
-							<button
-								className="p-2 bg-red-400 rounded-lg font-bold text-gray-900 flex items-center justify-center cursor-pointer w-full"
-								onClick={() => handleDelete(book?._id)}
-							>
-								<BookX /> Delete
-							</button>
-						</div>
+		<>
+			{loading ? (
+				<Loader />
+			) : (
+				<div className="max-w-screen-xl mx-auto flex flex-col md:flex-row md:justify-between p-10 gap-5">
+					<div className="flex justify-center md:w-1/3">
+						<img
+							src={book?.image}
+							alt={book?.title}
+						/>
 					</div>
-				)}
-			</div>
-		</div>
+					<div className="flex flex-col gap-5 text-gray-200 md:w-2/3">
+						<h1 className="text-3xl font-bold">{book?.title}</h1>
+						<h2 className="text-lg italic text-gray-400">
+							By {book?.author}
+						</h2>
+						<h3 className="text-lg">Category : {book?.category}</h3>
+						<h4 className="text-lg">
+							Published Year : {book?.publicationYear}
+						</h4>
+						<p className="text-lg text-justify">
+							{book?.description}
+						</p>
+						{isAdmin && (
+							<div className="flex justify-between w-full gap-10">
+								<div className="w-1/2">
+									<Link
+										to={`/admin/update-book/${book?._id}`}
+									>
+										<button className="p-2 bg-blue-400 rounded-lg font-bold text-gray-900 flex items-center justify-center cursor-pointer w-full">
+											<BookUp />
+											Update
+										</button>
+									</Link>
+								</div>
+								<div className="w-1/2">
+									<button
+										className="p-2 bg-red-400 rounded-lg font-bold text-gray-900 flex items-center justify-center cursor-pointer w-full"
+										onClick={() => handleDelete(book?._id)}
+									>
+										<BookX /> Delete
+									</button>
+								</div>
+							</div>
+						)}
+					</div>
+				</div>
+			)}
+		</>
 	)
 }
 
