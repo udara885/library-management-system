82804a265dca0afc721647f175caae37f5e56012
Udara Lakshan <ulakshan061@gmail.com>
2025-02-13 09:28:40 +0530

    fix: add loading state and Loader component to HomePage for improved data fetching experience

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,21 +1,35 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import BookCard from "../components/BookCard"
 import { useBookStore } from "../store/book"
+import Loader from "../components/Loader"
 
 const HomePage = () => {
 	const { getBooks, books } = useBookStore()
+	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
-		getBooks()
+		const fetchBooks = async () => {
+			setLoading(true)
+			await getBooks()
+			setLoading(false)
+		}
+		fetchBooks()
 	}, [getBooks])
 
 	return (
 		<div className="py-10 max-w-screen-xl mx-auto">
-			<div className="flex flex-wrap items-center justify-around gap-10 px-10">
-				{books.map((book) => (
-					<BookCard key={book._id} book={book} />
-				))}
-			</div>
+			{loading ? (
+					<Loader/>
+			) : (
+				<div className="flex flex-wrap items-center justify-around gap-10 px-10">
+					{books.map((book) => (
+						<BookCard
+							key={book._id}
+							book={book}
+						/>
+					))}
+				</div>
+			)}
 		</div>
 	)
 }
