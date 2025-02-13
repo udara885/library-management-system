595c30ecc77ee0fd159d6221cb54e15e7daaa398
Udara Lakshan <ulakshan061@gmail.com>
2025-02-13 13:53:26 +0530

    add: integrate GoBackBtn component into BookDetailPage for enhanced navigation

--- frontend/src/pages/BookDetailPage.tsx
+++ frontend/src/pages/BookDetailPage.tsx
@@ -7,6 +7,7 @@ import { AdminContext } from "../context/adminContext"
 import toast from "react-hot-toast"
 import { Book } from "../types/types"
 import Loader from "../components/Loader"
+import GoBackBtn from "../components/GoBackBtn"
 
 const BookDetailPage = () => {
 	const { id } = useParams()
@@ -55,55 +56,64 @@ const BookDetailPage = () => {
 	}
 
 	return (
-		<>
+		<div>
 			{loading ? (
 				<Loader />
 			) : (
-				<div className="max-w-screen-xl mx-auto flex flex-col md:flex-row md:justify-between p-10 gap-5">
-					<div className="flex justify-center md:w-1/3">
-						<img
-							src={book?.image}
-							alt={book?.title}
-						/>
-					</div>
-					<div className="flex flex-col gap-5 text-gray-200 md:w-2/3">
-						<h1 className="text-3xl font-bold">{book?.title}</h1>
-						<h2 className="text-lg italic text-gray-400">
-							By {book?.author}
-						</h2>
-						<h3 className="text-lg">Category : {book?.category}</h3>
-						<h4 className="text-lg">
-							Published Year : {book?.publicationYear}
-						</h4>
-						<p className="text-lg text-justify">
-							{book?.description}
-						</p>
-						{isAdmin && (
-							<div className="flex justify-between w-full gap-10">
-								<div className="w-1/2">
-									<Link
-										to={`/admin/update-book/${book?._id}`}
-									>
-										<button className="p-2 bg-blue-400 rounded-lg font-bold text-gray-900 flex items-center justify-center cursor-pointer w-full">
-											<BookUp />
-											Update
+				<div className="max-w-screen-xl mx-auto">
+					<GoBackBtn />
+					<div className="flex flex-col md:flex-row md:justify-between p-10 gap-5">
+						<div className="flex justify-center md:w-1/3">
+							<img
+								src={book?.image}
+								alt={book?.title}
+							/>
+						</div>
+						<div className="flex flex-col gap-5 text-gray-200 md:w-2/3">
+							<h1 className="text-3xl font-bold">
+								{book?.title}
+							</h1>
+							<h2 className="text-lg italic text-gray-400">
+								By {book?.author}
+							</h2>
+							<h3 className="text-lg">
+								Category : {book?.category}
+							</h3>
+							<h4 className="text-lg">
+								Published Year : {book?.publicationYear}
+							</h4>
+							<p className="text-lg text-justify">
+								{book?.description}
+							</p>
+							{isAdmin && (
+								<div className="flex justify-between w-full gap-10">
+									<div className="w-1/2">
+										<Link
+											to={`/admin/update-book/${book?._id}`}
+										>
+											<button className="p-2 bg-blue-400 rounded-lg font-bold text-gray-900 flex items-center justify-center cursor-pointer w-full">
+												<BookUp />
+												Update
+											</button>
+										</Link>
+									</div>
+									<div className="w-1/2">
+										<button
+											className="p-2 bg-red-400 rounded-lg font-bold text-gray-900 flex items-center justify-center cursor-pointer w-full"
+											onClick={() =>
+												handleDelete(book?._id)
+											}
+										>
+											<BookX /> Delete
 										</button>
-									</Link>
+									</div>
 								</div>
-								<div className="w-1/2">
-									<button
-										className="p-2 bg-red-400 rounded-lg font-bold text-gray-900 flex items-center justify-center cursor-pointer w-full"
-										onClick={() => handleDelete(book?._id)}
-									>
-										<BookX /> Delete
-									</button>
-								</div>
-							</div>
-						)}
+							)}
+						</div>
 					</div>
 				</div>
 			)}
-		</>
+		</div>
 	)
 }
 
