8af5db1c089cc1140c618c2f4beb6a94ad92f0b8
Udara Lakshan <ulakshan061@gmail.com>
2025-02-14 17:38:51 +0530

    add: implement member routes for CRUD operations

---
+++ backend/routes/member.routes.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import { addMember, deleteMember, getMember, getMembers, updateMember } from "../controller/member.controller"
+
+const router = express.Router()
+
+router.get( "/members", getMembers )
+router.get( "/member/:id", getMember )
+router.post( "/add-member", addMember )
+router.put( "/update-member/:id", updateMember )
+router.delete("/delete-member/:id", deleteMember)
+
+export default router
