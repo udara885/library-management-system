183639f2879f2fa8713df8ebc1ff9cf8ba6a532d
Udara Lakshan <ulakshan061@gmail.com>
2025-02-14 17:38:55 +0530

    add: implement rent routes for CRUD operations

---
+++ backend/routes/rent.routes.ts
@@ -0,0 +1,12 @@
+import express  from "express"
+import { addRent, deleteRent, getRent, getRents, updateRent } from "../controller/rent.controller"
+
+const router = express.Router()
+
+router.get( "/rents", getRents )
+router.get( "/rent/:id", getRent )
+router.post( "/add-rent", addRent )
+router.put( "/update-rent", updateRent )
+router.delete("/delete-rent", deleteRent)
+
+export default router
