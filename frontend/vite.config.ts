e6c8b3001f1aa8a08ba1cda21d1b6637fe1d74c5
Udara Lakshan <ulakshan061@gmail.com>
2025-02-14 18:51:13 +0530

    add: configure dotenv for environment variable management in Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +11,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:3000",
+				target: `${process.env.BACKEND_BASE_URL}`,
 			},
 		},
 	},
