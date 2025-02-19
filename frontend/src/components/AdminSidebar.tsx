54e6cd22067c2b8769c196d4498e863750e6a8c6
Udara Lakshan <ulakshan061@gmail.com>
2025-02-19 20:01:21 +0530

    refactor: simplify AdminSidebar component by removing setView prop and adding navigation links

--- frontend/src/components/AdminSidebar.tsx
+++ frontend/src/components/AdminSidebar.tsx
@@ -4,7 +4,7 @@ import { AdminContext } from "../context/adminContext"
 import { useContext } from "react"
 import { SidebarContext } from "../context/sidebarContext"
 
-const AdminSidebar = ({ setView }: { setView: (view: number) => void }) => {
+const AdminSidebar = () => {
 	const adminContext = useContext(AdminContext)
 
 	const sidebarContext = useContext(SidebarContext)
@@ -17,41 +17,41 @@ const AdminSidebar = ({ setView }: { setView: (view: number) => void }) => {
 		throw new Error("sidebarContext is undefined")
 	}
 
-  const { isAdmin, setIsAdmin } = adminContext
-  
-  const {setIsSidebarOpen} = sidebarContext
+	const { isAdmin, setIsAdmin } = adminContext
+
+	const { setIsSidebarOpen } = sidebarContext
 
 	return (
-		<div className="w-1/2 text-gray-200 bg-gray-800 fixed top-0 left-0 z-50 h-full flex flex-col items-center">
-			<span className="w-full flex justify-end p-4 cursor-pointer" onClick={() => setIsSidebarOpen(false)}>
-				<X size={30}/>
+		<div className="w-1/2 sm:w-1/4 text-gray-200 bg-gray-800 fixed top-0 left-0 z-50 h-full flex flex-col items-center justify-center">
+			<span
+				className="fixed top-0 right-1/2 sm:right-3/4 sm:p-5 p-3 cursor-pointer hover:text-red-400"
+				onClick={() => setIsSidebarOpen(false)}
+			>
+				<X size={30} />
 			</span>
 			<h1 className="text-2xl md:text-3xl font-semibold py-10">Menu</h1>
 			<ul className="flex flex-col gap-7 md:gap-10 text-xl md:text-2xl font-semibold px-10 py-4">
-				<li
-					className="flex gap-3 cursor-pointer items-center"
-					onClick={() => setView(1)}
-				>
-					<BookCopy />
-					Books
-				</li>
-				<li
-					className="flex gap-3 cursor-pointer items-center"
-					onClick={() => setView(2)}
-				>
-					<Users />
-					Members
-				</li>
-				<li
-					className="flex gap-3 cursor-pointer items-center"
-					onClick={() => setView(3)}
-				>
-					<BookUp2 />
-					Rent
-				</li>
+				<Link to={"/admin/books"}>
+					<li className="flex gap-3 cursor-pointer items-center hover:text-blue-400">
+						<BookCopy />
+						Books
+					</li>
+				</Link>
+				<Link to={"/admin/members"}>
+					<li className="flex gap-3 cursor-pointer items-center hover:text-blue-400">
+						<Users />
+						Members
+					</li>
+				</Link>
+				<Link to={"/admin/rents"}>
+					<li className="flex gap-3 cursor-pointer items-center hover:text-blue-400">
+						<BookUp2 />
+						Rent
+					</li>
+				</Link>
 				<Link to="/">
 					<li
-						className="flex gap-3 items-center"
+						className="flex gap-3 items-center hover:text-blue-400"
 						onClick={() => setIsAdmin(!isAdmin)}
 					>
 						<LogOut />
