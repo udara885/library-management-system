1b4d073606b551f2a4f844ae30097e82cfce6ca8
Udara Lakshan <ulakshan061@gmail.com>
2025-02-22 14:08:51 +0530

    feat: set default values for fromDate and toDate in rent model

--- backend/model/rent.model.ts
+++ backend/model/rent.model.ts
@@ -1,28 +1,32 @@
 import mongoose from "mongoose"
 
-const rentSchema = new mongoose.Schema( {
-  memberId: {
-    type: String,
-    require: true
-  },
-  bookId: {
-    type: String,
-    require:true
-  },
-  fromDate: {
-    type: String,
-    require: true
-  },
-  toDate: {
-    type: String,
-    require: true
-  },
-  status: {
-    type: String,
-    default: "Not returned"
-  }
-} )
+const rentSchema = new mongoose.Schema({
+	memberId: {
+		type: String,
+		require: true,
+	},
+	bookId: {
+		type: String,
+		require: true,
+	},
+	fromDate: {
+		type: String,
+		default: () => new Date().toISOString().split("T")[0],
+	},
+	toDate: {
+		type: String,
+		default: () => {
+			const date = new Date()
+			date.setDate(date.getDate() + 14)
+			return date.toISOString().split("T")[0]
+		},
+	},
+	status: {
+		type: String,
+		default: "Not returned",
+	},
+})
 
-const Rent = mongoose.model( "Rent", rentSchema )
+const Rent = mongoose.model("Rent", rentSchema)
 
 export default Rent
